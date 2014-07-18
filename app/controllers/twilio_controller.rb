100bf553a62784087d33ae29eb52d6f07cf60e56
jarodreyes <jarodreyes@gmail.com>
2014-07-18 12:01:26 -0700

    Add unit tests for TwilioController

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,6 +3,10 @@ require 'twilio-ruby'
 class TwilioController < ApplicationController
   include Webhookify
 
+  before_filter :authenticate_twilio_request, :only => [
+    :connect
+  ]
+
   # Render home page, embedding the Twilio number on the page (for mobile)
   def index
     @twilio_number = ENV['TWILIO_NUMBER']
@@ -54,6 +58,30 @@ class TwilioController < ApplicationController
     # Defined in webhookify.rb. Renders XML page.
     set_header
     render_twiml response
+  end
+
+
+  # Authenticate that all requests to our public-facing TwiML pages are
+  # coming from Twilio. Adapted from the example at 
+  # http://twilio-ruby.readthedocs.org/en/latest/usage/validation.html
+  # Read more on Twilio Security at https://www.twilio.com/docs/security
+  private
+  def authenticate_twilio_request
+    twilio_signature = request.headers['HTTP_X_TWILIO_SIGNATURE']
+
+    # Helper from twilio-ruby to validate requests. 
+    @validator = Twilio::Util::RequestValidator.new(ENV['TWILIO_AUTH_TOKEN'])
+ 
+    # the POST variables attached to the request (eg "From", "To")
+    # Twilio requests only accept lowercase letters. So scrub here:
+    post_vars = params.reject {|k, v| k.downcase == k}
+ 
+    is_twilio_req = @validator.validate(request.url, post_vars, twilio_signature)
+ 
+    unless is_twilio_req
+      render :xml => (Twilio::TwiML::Response.new {|r| r.Hangup}).text, :status => :unauthorized
+      false
+    end
   end
 
 end
