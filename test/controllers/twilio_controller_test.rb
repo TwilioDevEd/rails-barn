c3bbe8edf238e5da2f68185ad18643f11f0527d9
jarodreyes <jarodreyes@gmail.com>
2014-08-01 15:20:41 -0700

    Added comment to controller.

--- test/controllers/twilio_controller_test.rb
+++ test/controllers/twilio_controller_test.rb
@@ -19,7 +19,7 @@ class TwilioControllerTest < ActionController::TestCase
 
   test "should fail as fake Twilio request" do
     post(:connect, {'from' => "15008675309", 'to' => '12066505813'}, {'fake-signature' => "HpS7PBa1Agvt4OtO+wZp75IuQa0="})
-    assert_response 401
+    assert_response 401 #Unauthorized
   end
 
 end
